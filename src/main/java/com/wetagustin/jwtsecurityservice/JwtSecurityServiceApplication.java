ae7699aaaf7881422897c4871c7d463fbdae2e12
wet333 <wet.4gustin@gmail.com>
2025-06-28 15:25:42 -0300

    Initial commit, and PostgreSQL database compose file and credentials

---
+++ src/main/java/com/wetagustin/jwtsecurityservice/JwtSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wetagustin.jwtsecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSecurityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wetagustin/jwtsecurityservice/JwtSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wetagustin.jwtsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
